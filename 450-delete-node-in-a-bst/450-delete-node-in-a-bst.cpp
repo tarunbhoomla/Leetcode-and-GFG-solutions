54cbe6e88e23005468eaf157d2d4b18e7f9489f6
Tarun Bhoomla <70263030+tarunbhoomla@users.noreply.github.com>
2022-01-16 12:06:56 +0530

    Time: 88 ms (5.13%), Space: 32.8 MB (32.93%) - LeetHub

--- 450-delete-node-in-a-bst/450-delete-node-in-a-bst.cpp
+++ 450-delete-node-in-a-bst/450-delete-node-in-a-bst.cpp
@@ -11,47 +11,54 @@
  */
 class Solution {
 public:
-TreeNode *getSuccessor(TreeNode *curr)
-{
-    curr=curr->right;
-    while(curr!=NULL && curr->left!=NULL)
-        curr=curr->left;
-    return curr;
-}
-
-TreeNode* deleteNode(TreeNode *root, int x)
-{
-    if(root==NULL)
-        return root;
-    
-    if(root->val>x)
-        root->left=deleteNode(root->left,x);
-    
-    else if(root->val<x)
-        root->right=deleteNode(root->right,x);
-    
-    else{
-        if(root->left==NULL && root->right==NULL)
+TreeNode* deleteNode(TreeNode* root, int key) {
+        if(root==NULL)
+            return NULL;
+        
+        if(root->val >key)
+        {
+            root->left=deleteNode(root->left,key);
+            return root;
+        }
+        if(root->val<key)
+        {
+            root->right=deleteNode(root->right,key);
+           return root;
+        }
+       if(root->val==key)
+        {
+            if(root->left==NULL && root->right==NULL)
             {
                 delete root;
                 return NULL;
             }
-        else if(root->left==NULL){
-            TreeNode *temp=root->right;
-            delete root;
-            return temp;
-        }
-        else if(root->right==NULL){
-            TreeNode *temp=root->left;
-            delete root;
-            return temp;
-        }
-        else{
-            TreeNode *succ=getSuccessor(root);
-            root->val=succ->val;
-            root->right=deleteNode(root->right,succ->val);
-        }
-    }
-    return root;
-} 
+             if(root->right==NULL && root->left!=NULL)
+            {
+                TreeNode* temp =root->left;
+                delete root;
+                return temp;
+            }
+            if(root->left==NULL && root->right!=NULL)
+            {
+                TreeNode* temp =root->right;
+                delete root;
+                return temp;
+            }
+            
+            if(root->right!=NULL && root->left!=NULL)
+            {
+                TreeNode* replace=root->right;
+                while(replace->left!=NULL)
+                {
+                    replace=replace->left;
+                }
+                root->val=replace->val;
+                root->right = deleteNode(root->right,replace->val);
+                return root;
+                    
+            }
+            }
+        
+   return root;
+   }
 };
