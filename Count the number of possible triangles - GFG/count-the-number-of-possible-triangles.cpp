84cbdcf521e86421542d42322bb6ba5bd5394c2b
Tarun Bhoomla <70263030+tarunbhoomla@users.noreply.github.com>
2022-01-30 01:02:58 +0530

    Added solution - LeetHub

--- Count the number of possible triangles - GFG/count-the-number-of-possible-triangles.cpp
+++ Count the number of possible triangles - GFG/count-the-number-of-possible-triangles.cpp
@@ -12,15 +12,18 @@ int findNumberOfTriangles(int arr[], int n)
 {
     sort(arr,arr+n) ;
     int count = 0;
-    for (int i = 0; i < n - 2; ++i)
+    for(int i=n-1 ;i>=0 ;i--)
     {
-        int k = i + 2;
-        for (int j = i + 1; j < n; ++j) 
+        int l=0 ,h=i-1 ;
+        while(l < h)
         {
-            while (k < n && arr[i] + arr[j] > arr[k])
-                ++k;
-            if (k > j)
-                count += k - j - 1;
+            if(arr[l]+arr[h] > arr[i])
+             {
+                 count += h-l ;
+                 h-- ;
+             }
+             else
+              l++ ;
         }
     }
  
