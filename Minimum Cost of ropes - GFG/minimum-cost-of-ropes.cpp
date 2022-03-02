869b28f4aabe2b0a739274e6b89d23b9e98e2033
Tarun Bhoomla <70263030+tarunbhoomla@users.noreply.github.com>
2022-03-02 19:14:30 +0530

    Added solution - LeetHub

--- Minimum Cost of ropes - GFG/minimum-cost-of-ropes.cpp
+++ Minimum Cost of ropes - GFG/minimum-cost-of-ropes.cpp
@@ -10,25 +10,46 @@ class Solution
     //Function to return the minimum cost of connecting the ropes.
     long long minCost(long long arr[], long long n) 
     {
-        multiset<long long>s ;
-        for(long long i=0 ;i<n ;i++)
-          s.insert(arr[i]) ;
+    //     multiset<long long>s ;
+    //     for(long long i=0 ;i<n ;i++)
+    //         s.insert(arr[i]) ;
           
-        long long res = 0 ;
-        long long x=0 ;
-        long long y=0 ;
-        while(s.size()!=1)
-        {
-            x = *s.begin() ;
-            s.erase(s.begin()); 
-            y = *s.begin() ;
-            s.erase(s.begin()) ;
+    //     long long res = 0 ;
+    //     long long x=0 ;
+    //     long long y=0 ;
+    //     while(s.size()!=1)
+    //     {
+    //         x = *s.begin() ;
+    //         s.erase(s.begin()); 
+    //         y = *s.begin() ;
+    //         s.erase(s.begin()) ;
             
-            s.insert(x+y); 
-            res += x+y ;
-        }
+    //         s.insert(x+y); 
+    //         res += x+y ;
+    //     }
         
-        return res ;
+    //     return res ;
+    // }
+    
+    priority_queue<long long ,vector<long long>,greater<long long>>pq ;
+    for(long long i=0 ;i< n;i++)
+      pq.push(arr[i]) ;
+    long long cost = 0 ;
+    long long x ;
+    while(!pq.empty())
+    {
+        x = pq.top() ;
+        pq.pop() ;
+        if(pq.empty())
+           return cost ;
+        long long y = pq.top() ;
+        pq.pop() ;
+        x = x+y ;
+        cost += x ;
+        pq.push(x) ;
+    }
+      
+      return cost ;
     }
 };
 
