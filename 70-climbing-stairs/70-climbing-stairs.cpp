8ba91db0d2714a188424c38bd39f956e996a75a9
Tarun Bhoomla <70263030+tarunbhoomla@users.noreply.github.com>
2022-03-15 22:44:03 +0530

    Time: 5 ms (10.49%), Space: 6 MB (54.82%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,26 +1,47 @@
     
 class Solution {
 private:
-    int dp[46] ;
-public:
     
-    int recursive(int n)
-    {          
-        if(n<0)
-            return 0 ;   
-        if(n==0)
-            return 1 ;  
+public:
+// int dp[46] ;
+//     int recursive(int n)
+//     {          
+//         if(n<0)
+//             return 0 ;   
+//         if(n==0)
+//             return 1 ;  
         
-        if(dp[n]!=-1)
-            return dp[n] ;
+//         if(dp[n]!=-1)
+//             return dp[n] ;
         
-      dp[n] = recursive(n-1) + recursive(n-2) ;  
+//       dp[n] = recursive(n-1) + recursive(n-2) ;  
       
-      return dp[n] ;
-    }
-    int climbStairs(int n) 
+//       return dp[n] ;
+//     }
+//     int climbStairs(int n) 
+//     {
+//         memset(dp,-1 ,sizeof(dp)) ;
+//         return recursive(n) ;
+//     }
+    
+    int climbStairs(int n)
     {
-        memset(dp,-1 ,sizeof(dp)) ;
-        return recursive(n) ;
+         if(n==0)
+               return 1 ;
+         if(n==1)
+              return 1 ;
+        if(n==2)
+              return 2 ;
+        int x = 1 ;
+        int y = 2 ;
+        
+        for(int i=3 ;i<=n ;i++)
+        {
+            int curr = x+y ;
+            x = y ;
+            y = curr ;
+        }
+        
+        return y ;
     }
 };
