073fbb13c3a65e4f15dfaad661bd457847081c0e
Tarun Bhoomla <70263030+tarunbhoomla@users.noreply.github.com>
2022-03-15 22:31:29 +0530

    Time: 2 ms (32.03%), Space: 5.9 MB (82.91%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -0,0 +1,24 @@
+    int dp[46] ;
+class Solution {
+public:
+
+    int recursive(int n)
+    {          
+        if(n<0)
+            return 0 ;   
+        if(n==0)
+            return 1 ;  
+        
+        if(dp[n]!=-1)
+            return dp[n] ;
+        
+      dp[n] = recursive(n-1) + recursive(n-2) ;  
+      
+      return dp[n] ;
+    }
+    int climbStairs(int n) 
+    {
+        memset(dp,-1 ,sizeof(dp)) ;
+        return recursive(n) ;
+    }
+};
