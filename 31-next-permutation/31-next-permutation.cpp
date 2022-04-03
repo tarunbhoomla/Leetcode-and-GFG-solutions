1855b328f85aa95f92963811d83eac70ff59401b
Tarun Bhoomla <70263030+tarunbhoomla@users.noreply.github.com>
2022-04-03 15:35:30 +0530

    Time: 7 ms (51.84%), Space: 12.1 MB (75.54%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -1,70 +1,41 @@
 class Solution {
 public:
-//     void fun(vector<int>&nums ,int curr , vector<vector<int>>&res)
-//     {
-//         if(curr==nums.size() )
-//             res.push_back(nums) ;
-//         for(int i=curr ;i < nums.size() ;i++)
-//         {
-//             swap(nums[i] ,nums[curr]) ;
-//             fun(nums,curr+1 ,res) ;
-//             swap(nums[i] ,nums[curr]) ;
-//         }
-//     }
-//     void nextPermutation(vector<int>& nums) 
-//     {
-//         vector<vector<int>>res ;
-//         fun(nums ,0 , res ) ;
-//         sort(res.begin() ,res.end() ) ;
-//         int n = res.size() ;
-//         int pos ;
-//         for(int i=0 ;i<n ;i++)
-//         {
-//             if(res[i] == nums)
-//                   pos = i ;
-//         }
-//         if(pos == n-1)
-//             nums = res[0] ;
-//         else
-//             nums = res[pos+1] ;
-      
-//     }
-         void nextPermutation(vector<int>& nums) 
-         {
-             int pos1 = INT_MIN ;
-             for(int i=nums.size()-2 ; i>=0 ;i--)
-             {
-                 if(nums[i] < nums[i+1]) 
-                 {
-                     pos1 = i ;
-                     break ;
-                 }
-             }
-             if(pos1 == INT_MIN)
-             {
-                 reverse(nums.begin(),nums.end()) ;
-                 return ;
-             }
-             int n = nums.size() ;
-              if(nums[n-1]>nums[n-2])
-             { 
-              swap(nums[n-1], nums[n-2]);
-              return ;
-             }
-             
-             int pos2 = INT_MIN ;
-             for(int i=nums.size()-1 ;i>=0 ;i--)
-             {
-                 if(nums[i] > nums[pos1])
-                 {
-                     pos2 = i ;
-                     break ;
-                 }
-             }
-                   
-             swap(nums[pos1] , nums[pos2]) ;
-             
-             sort(nums.begin()+pos1+1 , nums.end()) ;
-             
-         }
+    void nextPermutation(vector<int>& nums) 
+    {
+        int temp1 ;
+        int n = nums.size()-1 ;
+        bool flag = false ;
+        for(int i=n-1 ;i>=0 ;i--)
+        {
+            if(nums[i] < nums[i+1])
+            {
+                flag = true ;
+                temp1 = i ;
+                break ;
+            }
+        }
+        if(flag==false)
+        {
+            reverse(nums.begin() ,nums.end()) ;
+            return ;
+        }
+        int minG = INT_MAX ;
+        for(int i=n ;i> temp1 ;i--)
+        {
+            if(nums[i]>nums[temp1])
+            {
+                minG = min(minG , nums[i]) ;
+            }
+        }
+        int temp2 ;
+        for(int i=0 ;i<=n ;i++)
+        {
+            if(nums[i]==minG)
+                 temp2 = i ;
+        }
+        swap(nums[temp1] , nums[temp2]) ;
+        reverse(nums.begin()+temp1+1 , nums.end()) ;
+        
+        
+    }
 };
