7fd05b4c0e2f473b20828ca0dc64fb736ea194ea
Tarun Bhoomla <70263030+tarunbhoomla@users.noreply.github.com>
2022-07-20 11:40:48 +0530

    Time: 222 ms (11.30%), Space: 51.3 MB (21.87%) - LeetHub

---
+++ 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maximumElementAfterDecrementingAndRearranging(vector<int>& arr)
+    {
+        sort(arr.begin() ,arr.end()) ;
+        arr[0] = 1 ;
+        int n = arr.size() ;
+        for(int i=1 ;i<arr.size() ;i++)
+        {
+            if(arr[i] != arr[i-1])
+                arr[i] = arr[i-1]+1 ;
+        }
+        
+        return arr[n-1] ;
+    }
+};
