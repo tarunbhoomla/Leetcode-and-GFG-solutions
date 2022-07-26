6b0c2155e31946cac52b95e58fcf0dd9f7351961
Tarun Bhoomla <70263030+tarunbhoomla@users.noreply.github.com>
2022-07-26 10:45:43 +0530

    Time: 11 ms (68.40%), Space: 13.6 MB (83.66%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -2,25 +2,62 @@ class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) 
     {        
-        int j=-1 ,k=-1 ;
-        for(int i=0 ;i<nums.size() ;i++)
+//         int j=-1 ,k=-1 ;
+//         for(int i=0 ;i<nums.size() ;i++)
+//         {
+//             if(nums[i]==target)
+//             {
+//                 j=i ;
+//                 break ;
+//             }
+//         }
+        
+//         for(int i=nums.size()-1 ;i>=0 ;i--)
+//         {
+//             if(nums[i]==target)
+//             {
+//                 k=i ;
+//                 break; 
+//             }
+//         }
+        
+//         return {j,k} ;
+        
+        int l=0 , h=nums.size()-1 ;
+        int first=-1 ,last=-1 ;
+        while(l<=h)
         {
-            if(nums[i]==target)
+            int mid = l+(h-l)/2 ;
+            
+            if((nums[mid]==target && mid==0) || nums[mid]==target && nums[mid-1]!=target)
             {
-                j=i ;
+                first = mid ;
                 break ;
-            }
+            } 
+            
+            if(nums[mid] < target)
+                l = mid+1 ;
+            else if(nums[mid] > target || nums[mid]==target)
+                h = mid-1 ;   
         }
-        
-        for(int i=nums.size()-1 ;i>=0 ;i--)
+
+        l=0 , h=nums.size()-1 ;
+        while(l<=h)
         {
-            if(nums[i]==target)
+            int mid = l+(h-l)/2 ;
+            
+            if((nums[mid]==target && mid==nums.size()-1) || nums[mid]==target && nums[mid+1]!=target)
             {
-                k=i ;
-                break; 
-            }
+                last = mid ;
+                break ;
+            } 
+            
+            if(nums[mid] < target || nums[mid]==target)
+                l = mid+1 ;
+            else if(nums[mid] > target)
+                h = mid-1 ;   
         }
         
-        return {j,k} ;
+        return {first,last} ;
     }
 };
