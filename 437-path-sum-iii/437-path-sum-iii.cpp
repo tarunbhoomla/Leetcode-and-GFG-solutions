1cdae2c6d9cc767bd0993ade3535370e75298ae5
Tarun Bhoomla <70263030+tarunbhoomla@users.noreply.github.com>
2022-08-08 16:44:27 +0530

    Time: 33 ms (63.19%), Space: 19.3 MB (33.70%) - LeetHub

--- 437-path-sum-iii/437-path-sum-iii.cpp
+++ 437-path-sum-iii/437-path-sum-iii.cpp
@@ -11,29 +11,60 @@
  */
 class Solution {
 public:
-    int res = 0 ;
-    int fun(TreeNode* root , int targetSum ,long long int curr = 0)
-    {
-        if(!root)
-            return 0 ;
+//     int res = 0 ;
+//     int fun(TreeNode* root , int targetSum ,long long int curr = 0)
+//     {
+//         if(!root)
+//             return 0 ;
         
-        curr += root->val ;
-        if(curr == targetSum)
-        {
-            res++ ;
-        }
-        return fun(root->left, targetSum ,curr) + fun(root->right , targetSum ,curr) ;
-    }
-    int pathSum(TreeNode* root, int targetSum)
-    {
-        if(!root)
-            return 0 ;
+//         curr += root->val ;
+//         if(curr == targetSum)
+//         {
+//             res++ ;
+//         }
+//         return fun(root->left, targetSum ,curr) + fun(root->right , targetSum ,curr) ;
+//     }
+//     int pathSum(TreeNode* root, int targetSum)
+//     {
+//         if(!root)
+//             return 0 ;
         
-        int left = pathSum(root->left ,targetSum) ;
-        int right = pathSum(root->right ,targetSum) ;
+//         int left = pathSum(root->left ,targetSum) ;
+//         int right = pathSum(root->right ,targetSum) ;
         
-        int x = fun(root , targetSum) ;
+//         int x = fun(root , targetSum) ;
         
-        return res ;
-    }
+//         return res ;
+//     }
+       void fun(TreeNode* root ,int target , unordered_map<long long,int>&mp ,int &res ,long long curr)
+       {
+           if(!root)
+               return ;
+           
+           curr += root->val ;
+           
+           if(mp.find(curr-target) != mp.end())
+           {
+               res += mp[curr-target] ;
+           }
+           mp[curr]++ ;
+           
+           fun(root->left ,target ,mp ,res ,curr) ;
+           fun(root->right ,target ,mp ,res ,curr) ;
+           
+           mp[curr]-- ;
+           // curr -= root->val ;
+       }
+    
+       int pathSum(TreeNode* root, int targetSum)
+       {
+           if(!root)
+               return 0 ;
+           unordered_map<long long,int>mp ;
+           mp[0]=1 ;
+           int res = 0 ;
+           fun(root ,targetSum ,mp ,res ,0) ;
+           
+           return res ;
+       }
 };
